f693ecb86318a9c4ebbf816513e6f340b98a057c
Mihir Dasgupta <dasguptamihir10@gmail.com>
2022-03-25 13:24:28 +0530

    add skt to runMain

--- runMain.py
+++ runMain.py
@@ -32,7 +32,8 @@ def main():
 		[(generate_precipitationRate_profs,year,month,date,monthConversion),(plot_precipitationRate,err)], # 4 --> Precipitation Rate Contours
 		[(generate_relHumidity_profs,year,month,date,monthConversion),(plot_relHumidity,err)], # 5 --> Relative Humidity Contours
 		[(generate_specHumidity_profs,year,month,date,monthConversion),(plot_specHumidity,err)], # 6 --> Specific Humidity Contours.
-		[((generate_cloudCover,year,month,date,monthConversion)),(plot_cloudCover, err)] # 7 --> Cloud Cover Contours
+		[(generate_cloudCover,year,month,date,monthConversion),(plot_cloudCover, err)], # 7 --> Cloud Cover Contours
+		[(generate_skinTemperature,year,month,date,monthConversion)),(plot_skinTemperature,err)] # 8 --> Skin Temperature Contours.
 	]
 	for index in chosenData:
 		for fn in functions[index]:
